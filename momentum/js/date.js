bc705c04711e06fc42ab9c2a04365d1cb51cd2db
freenokke <kosmatkov94@gmail.com>
2021-10-22 09:49:56 +0300

    feat: add date display

--- momentum/js/date.js
+++ momentum/js/date.js
@@ -1,17 +1,19 @@
 export default function dateTime() {
     const time = document.querySelector('.time');
+    const dateTag = document.querySelector('.date');
 
 function showTime() {
     const date = new Date();
+    //вывод времени
     const currentTime = date.toLocaleTimeString();
     time.innerHTML = currentTime;
+    //вывод даты
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
+    const currentDate = date.toLocaleDateString('en-En', options);
+    dateTag.textContent = currentDate;
     setTimeout(showTime, 1000);
 }
 showTime();
-
-//вывод даты
-
-const dateTag = document.querySelector('.date');
 
 
 
